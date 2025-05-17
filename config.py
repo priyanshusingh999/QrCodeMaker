dbc121ea146af96e6cf54a8da4cca9c2aa0bc15e
Priyanshu Singh Rajput <singpriyanshu24+github@gmail.com>
2025-05-17 17:47:52 +0530

    -------------------

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 # OWNER_ID = 
 # FORCE_JOIN_CHANNEL = ''  # Add the channel username or ID here
 
-BOT_TOKEN = os.getenv('')
-OWNER_ID = int(os.getenv(''))
-FORCE_JOIN_CHANNEL = os.getenv('')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+OWNER_ID = int(os.getenv("OWNER_ID", ""))  # Default to 0 if not set
+FORCE_JOIN_CHANNEL = os.getenv('FORCE_JOIN_CHANNEL')
